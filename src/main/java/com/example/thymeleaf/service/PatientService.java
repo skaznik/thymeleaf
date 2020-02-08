863d3758a1ee7a0e399ecd8a3c22dbabf85c3e07
skaznik <skaznik@wszib.edu.pl>
2020-02-08 16:18:54 +0100

    1

--- src/main/java/com/example/thymeleaf/service/PatientService.java
+++ src/main/java/com/example/thymeleaf/service/PatientService.java
@@ -1,10 +1,25 @@
 package com.example.thymeleaf.service;
 
+import com.example.thymeleaf.model.SkierowanieDoLekarza;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
+import java.util.*;
 
 @Component
 public class PatientService {
-//private Map<Integer, Patient> PatientMap = new HashMap<>();
+private Map<Integer, SkierowanieDoLekarza> PatientMap = new HashMap<>();
+
+public SkierowanieDoLekarza createSkierowanie(String lekarz, String pacjent, Date termin) {
+    int id = new Random().nextInt();
+    SkierowanieDoLekarza skierowanieDoLekarza = new SkierowanieDoLekarza(id, lekarz, pacjent, termin);
+    PatientMap.put(id,skierowanieDoLekarza);
+    return skierowanieDoLekarza;
+}
+public Collection<SkierowanieDoLekarza> listaSkierowan() {
+    return PatientMap.values();
+    }
+
+    public SkierowanieDoLekarza getSkierowanie(int id) {
+    return PatientMap.get(id);
+    }
 }
