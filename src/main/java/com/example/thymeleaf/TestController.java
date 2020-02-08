9b42ca50a29403138d6d340eedf02fb7c1f75727
skaznik <skaznik@wszib.edu.pl>
2020-02-08 11:13:34 +0100

    1

--- src/main/java/com/example/thymeleaf/TestController.java
+++ src/main/java/com/example/thymeleaf/TestController.java
@@ -1,5 +1,6 @@
 package com.example.thymeleaf;
 
+import com.example.thymeleaf.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TestController {
     @GetMapping("/test")
     public String test(Model model) {
+        User user = new User(11,"Adam", "Skaźnik", 43);
+        model.addAttribute("user", user);
+        model.addAttribute("styl", "color: red;");
         return "test-app";
     }
 
---
+++ src/main/java/com/example/thymeleaf/model/User.java
@@ -0,0 +1,48 @@
+package com.example.thymeleaf.model;
+
+public class User {
+
+    int id;
+    String imie;
+    String nazwisko;
+    int wiek;
+
+    public User(int id, String imie, String nazwisko, int wiek) {
+        this.id = id;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wiek = wiek;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(int wiek) {
+        this.wiek = wiek;
+    }
+}
