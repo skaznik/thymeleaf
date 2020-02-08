47eb89ed1586043a408cad9a06be248e21304a98
skaznik <skaznik@wszib.edu.pl>
2020-02-08 14:37:48 +0100

    1

--- src/main/java/com/example/thymeleaf/TestController.java
+++ src/main/java/com/example/thymeleaf/TestController.java
@@ -5,13 +5,19 @@ import com.example.thymeleaf.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TestController {
 
+    public class NotFoundException extends RuntimeException {
+
+    }
     @Autowired
     UserService userService;
     @GetMapping("/listUsers")
@@ -27,6 +33,16 @@ public class TestController {
         return "list-users-view";
     }
 
+    @GetMapping("/getUser/{id}")
+    public String getUser(@PathVariable int id, Model model) {
+        User user = userService.getUser(id);
+        if(user == null) {
+            throw new NotFoundException();
+        }
+        model.addAttribute("user", user);
+        return "user-details";
+    }
+
     @GetMapping("/addUser")
     public String addUser(Model model) {
         model.addAttribute("user", new User());
@@ -34,9 +50,28 @@ public class TestController {
     }
 
     @PostMapping("/addUser")
-    public String createUser(@ModelAttribute User user, Model model) {
+    public String createUser(@ModelAttribute User user, BindingResult bindingResult, Model model) {
+        validate(user, bindingResult);
+            if(bindingResult.hasErrors()) {
+                model.addAttribute("user", user);
+            return "add-user";
+            }
         userService.createUser(user.getImie(), user.getNazwisko(), user.getWiek());
         return "redirect:/listUsers";
     }
+
+    public void validate(User user, BindingResult bindingResult) {
+        if (user.getImie() == null || user.getImie().isEmpty()) {
+            bindingResult.addError(new ObjectError("imie", "MUSISZ PODAĆ IMIĘ"));
+        }
+        if (user.getNazwisko() == null || user.getNazwisko().isEmpty()) {
+            bindingResult.addError(new ObjectError("nazwisko", "MUSISZ PODAĆ NAZWISKO"));
+        }
+        if (user.getWiek() < 18) {
+            bindingResult.addError(new ObjectError("wiek", "MUSISZ BYĆ PEŁNOLETNI!!!!"));
+        }
+
+    }
+
 
 }
